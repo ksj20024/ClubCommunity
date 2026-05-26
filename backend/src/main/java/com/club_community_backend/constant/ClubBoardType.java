bc14069d3ee4a43c647b19c93b998d302423c4d9
ksj20024 <ksj05094@naver.com>
2026-05-26 20:21:44 +0900

    동아리 게시판 관리용 게시판 타입(FREE, ACTIVITY, NOTICE) 추천/비추천 플래그(VOTE, DOWNVOTE)

---
+++ backend/src/main/java/com/club_community_backend/constant/ClubBoardType.java
@@ -0,0 +1,7 @@
+package com.club_community_backend.constant;
+
+public enum ClubBoardType {
+    FREE,       // 자유 게시판
+    ACTIVITY,   // 활동 게시판 (사진/동영상 위주)
+    NOTICE      // 공지사항 (운영진만 작성 가능하도록 서비스에서 통제)
+}
---
+++ backend/src/main/java/com/club_community_backend/constant/VoteType.java
@@ -0,0 +1,6 @@
+package com.club_community_backend.constant;
+
+public enum VoteType {
+    UPVOTE,   // 추천
+    DOWNVOTE  // 비추천
+}
