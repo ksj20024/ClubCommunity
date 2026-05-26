40d19cb523c3a3dbefe5c937cb7960a86e248fc7
ksj20024 <ksj05094@naver.com>
2026-05-26 20:24:23 +0900

    세션 감시용 어노테이션 추가

--- backend/src/main/java/com/club_community_backend/ClubCommunityBackendApplication.java
+++ backend/src/main/java/com/club_community_backend/ClubCommunityBackendApplication.java
@@ -3,12 +3,14 @@ package com.club_community_backend;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.data.web.config.EnableSpringDataWebSupport;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @EnableJpaAuditing
 @SpringBootApplication
+@EnableSpringDataWebSupport(pageSerializationMode = EnableSpringDataWebSupport.PageSerializationMode.VIA_DTO)
 public class ClubCommunityBackendApplication {
 
     @RequestMapping("/")
