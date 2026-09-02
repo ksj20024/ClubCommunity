f2c3532ef5e930b83dd598e9bf072a791c729cf3
ksj20024 <ksj05094@naver.com>
2026-09-02 19:27:59 +0900

    동아리 가입 제한 용도의 이메일 필드 추가 동아리 전체 조회 용도의 클래스 생성 유저 가입시 리턴할 정보 클래스 생성

--- backend/src/main/java/com/club_community_backend/dto/ClubDto.java
+++ backend/src/main/java/com/club_community_backend/dto/ClubDto.java
@@ -32,6 +32,7 @@ public class ClubDto {
         private EnumSet<ClubJoinMethodRole> joinType;
         private ClubTypeRole clubType;
         private String studentNo;
+        private String email;
     }
 
     @Getter @Builder
@@ -56,6 +57,27 @@ public class ClubDto {
         private String clubPassword; // 필요 시 마스킹 처리하거나 제외
         private EnumSet<ClubJoinMethodRole> joinType;
         private ClubTypeRole clubType;
+    }
+
+    @Getter @Builder
+    public static class UserJoinedResponse {
+        private Long id;
+        private String clubName;
+        private String schoolName;    // 연합 동아리일 경우 null이 될 수 있음
+        private ClubTypeRole clubType; // 일반 / 학교 / 연합
+    }
+
+    @Getter
+    @Builder
+    @AllArgsConstructor
+    @NoArgsConstructor
+    public static class ListResponse {
+        private Long id;
+        private String clubName;
+        private String schoolName;
+        private ClubTypeRole clubType;
+        private EnumSet<ClubJoinMethodRole> clubJoinMethod;
+        // 필요하다면 동아리 소개글이나 카테고리 등의 필드 추가
     }
 
 }
