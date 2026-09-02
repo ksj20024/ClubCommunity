dada0ef267e5a0357e774a3f1a22c66a44994033
ksj20024 <ksj05094@naver.com>
2026-09-02 16:34:58 +0900

    댓글 DB 구조 설정 및 검색 옵션 추가

---
+++ backend/src/main/java/com/club_community_backend/entity/CommentEntity.java
@@ -0,0 +1,27 @@
+package com.club_community_backend.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "comments")
+public class CommentEntity extends BaseTimeEntity {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "post_id", nullable = false)
+    private PostEntity post;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private UserEntity user;
+
+    @Column(nullable = false, length = 500)
+    private String content;
+}
